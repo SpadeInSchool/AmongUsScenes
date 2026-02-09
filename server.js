c9073b5b83c16f7085d829d8e0646618f0afa703
SpadeInSchool <151034857+SpadeInSchool@users.noreply.github.com>
2026-02-09 09:18:39 -0500

    Update and rename server.json to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 2001;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
